5e5e123dc6afc46e1fe4dd1a022ccbbbb8888316
José Lopes <162456984+onitech38@users.noreply.github.com>
2026-03-09 02:37:12 +0000

    Update config.js

--- js/config.js
+++ js/config.js
@@ -24,7 +24,7 @@ const CONFIG = {
     musica: {
       ativo: true,
       endpoint: "https://api-inference.huggingface.co/models/facebook/musicgen-small",
-      token: "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",
+      token: "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",
       mensagem: '🎵 A gerar música via Hugging Face (proxy)…',
     },
 
